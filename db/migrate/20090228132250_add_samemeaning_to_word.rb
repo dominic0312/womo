d926dce305eea099eb3690849b325be9f1f2a915
dominic0312 <root@Dominic-Dev.(none)>
2009-03-02 14:09:34 +0900

    del a migratation

--- db/migrate/20090228132250_add_samemeaning_to_word.rb
+++
@@ -1,9 +0,0 @@
-class AddSamemeaningToWord < ActiveRecord::Migration
-  def self.up
-    add_column :words, :samemeaning, :word
-  end
-
-  def self.down
-    remove_column :words, :samemeaning
-  end
-end
